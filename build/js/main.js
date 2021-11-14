eff0ed886a398acfd280ce235050f54f6d70fcb1
orangeBisquit <62883132+orangeBisquit@users.noreply.github.com>
2021-11-14 13:45:09 +0600

    footer finished

--- build/js/main.js
+++ build/js/main.js
@@ -1,2 +1,16 @@
 import './modules/mobile-menu.js';
 import './modules/accordeon.js';
+
+const footerEmail = document.querySelector('#sub-email');
+
+footerEmail.addEventListener('focusout', () => {
+  if (footerEmail.value.length > 0) {
+    footerEmail.style.paddingLeft = '10px';
+  } else {
+    footerEmail.style.paddingLeft = '0';
+  }
+});
+
+footerEmail.addEventListener('focusin', () => {
+  footerEmail.style.paddingLeft = '10px';
+});
