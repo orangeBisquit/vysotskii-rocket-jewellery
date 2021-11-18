feabc7a634b9f3f0a43900ba5d0f88edd1f00b00
orangeBisquit <62883132+orangeBisquit@users.noreply.github.com>
2021-11-18 14:04:34 +0600

    mentor review fixes

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,5 @@
 import './modules/mobile-menu.js';
+import './modules/slider-setup.js';
 import './modules/accordeon.js';
 import './modules/footer-input.js';
 import './modules/filter.js';
--- source/js/modules/js-disabled.js
+++
@@ -1,7 +0,0 @@
-const body = document.querySelector('body');
-const mobileMenu = document.querySelector('.mobile-menu');
-
-window.addEventListener('load', () => {
-  body.classList.remove('js-disabled');
-  mobileMenu.classList.remove('mobile-menu--active');
-});
